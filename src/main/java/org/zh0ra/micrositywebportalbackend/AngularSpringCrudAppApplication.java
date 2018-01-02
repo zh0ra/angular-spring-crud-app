2a825cc81c8f2fdd5748519fa406058f79c2291e
zhora <zhora@localhost.localdomain>
2018-01-02 07:48:11 +0100

    Ok

---
+++ src/main/java/org/zh0ra/micrositywebportalbackend/AngularSpringCrudAppApplication.java
@@ -0,0 +1,12 @@
+package org.zh0ra.micrositywebportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringCrudAppApplication.class, args);
+	}
+}
---
+++ src/test/java/org/zh0ra/micrositywebportalbackend/AngularSpringCrudAppApplicationTests.java
@@ -0,0 +1,16 @@
+package org.zh0ra.micrositywebportalbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AngularSpringCrudAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
